f2a8d0ebac0b6930f74dabf86760fcbd69932685
Rifki Fauzi <versitani@gmail.com>
2019-02-27 18:03:02 +0700

    Update data.js

--- cartogram/src/data.js
+++ cartogram/src/data.js
@@ -1,7 +1,7 @@
 var petaku = L.map('kartogram').setView([-8.404913, 115.072245],10);
 
-var latarpeta = L.tileLayer('https://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+var latarpeta = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>''
 }).addTo(petaku);
 
 
